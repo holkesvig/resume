10629deb4199f9424ae8ce61075b23dcba3cc242
Melissa Nelson <melissa.e.nelson@target.com>
2021-06-01 18:11:15 -0500

    responsive updates

--- sections/Home/Home.js
+++ sections/Home/Home.js
@@ -2,7 +2,7 @@ import styles from './Home.module.scss'
 export default function Home() {
   return (
     <div className={styles.home}>
-      <div className={styles.leftContent}><img className={styles.selfPortrait} src="/images/self-portrait.png"></img></div>
+      <div className={styles.leftContent}><img className={styles.portrait} src="/images/self-portrait.png"></img></div>
       <div className={styles.rightContent}><h1>Hi, I'm Melissa.</h1>
       <p>I'm a frontend software engineer. I care deeply about user experience and product design, 
         and I'm seeking to bridge the gap between elegance and efficiency in enterprise and consumer web applications. </p>
