b003fd6bd866a23f4b491726c93e83d56bbbb40d
Melissa Nelson <melissa.e.nelson@target.com>
2021-06-01 19:28:56 -0500

    update home copy

--- sections/Home/Home.js
+++ sections/Home/Home.js
@@ -4,8 +4,9 @@ export default function Home() {
     <div className={styles.home}>
       <div className={styles.leftContent}><img className={styles.portrait} src="/images/self-portrait.png"></img></div>
       <div className={styles.rightContent}><h1>Hi, I'm Melissa.</h1>
-      <p>I'm a frontend software engineer who is deeply passionate about user experience and product design, 
-        and I'm seeking to bridge the gap between elegance and efficiency in enterprise and consumer web applications. </p>
+      <p>I'm a front-end software engineer who is deeply passionate about user experience and product design. 
+         My goal is to bridge the gap between elegance and efficiency in enterprise and consumer web applications 
+         while creating a digital world that is reliable, accessible, and equitable for all.</p>
         <p>Let's build teams that take a user-first approach to engineering.</p>
       </div>
     </div>
