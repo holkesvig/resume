2200486b041506d4ecf5b2ab56b3034813018ab5
Melissa Nelson <melissa.e.nelson@target.com>
2021-06-02 00:23:15 -0500

    add social media links with placeholder icon

--- sections/Home/Home.js
+++ sections/Home/Home.js
@@ -1,4 +1,7 @@
 import styles from './Home.module.scss'
+import { Button } from '@material-ui/core'
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <div className={styles.home}>
@@ -6,11 +9,19 @@ export default function Home() {
         <img className={styles.portrait} 
         src="/images/self-portrait.png" alt="A cartoonized self-portrait of Melissa Nelson using a Macbook"></img>
       </div>
-      <div className={styles.rightContent}><h1>Hi, I'm Melissa.</h1>
-      <p>I'm a front-end software engineer who is deeply passionate about user experience and product design. 
-         My purpose is to bridge the gap between elegance and performance in enterprise and consumer web applications 
-         while creating a digital society that is reliable, accessible, and equitable for all.</p>
-        <p>Let's build teams that employ a user-first approach to engineering.</p>
+      <div className={styles.rightContent}>
+          <div className={styles.top}>
+            <h1>Hi, I'm Melissa.</h1>
+            <p>I'm a front-end software engineer who is deeply passionate about user experience and product design. 
+                My purpose is to bridge the gap between elegance and performance in enterprise and consumer web applications 
+                while creating a digital society that is reliable, accessible, and equitable for all.</p>
+            <p>Let's build teams that employ a user-first approach to engineering.</p>
+        </div>
+        <div className={styles.bottom}>
+          <Button className={styles.socialButton}><a href="https://linkedin.com/in/holkesvig" target="blank"><img src="http://simpleicon.com/wp-content/uploads/rocket.png" alt="Linkedin Icon" className={styles.socialIcon}/></a></Button>
+          <Button className={styles.socialButton}><a href="https://github.com/holkesvig" target="blank"><img src="http://simpleicon.com/wp-content/uploads/rocket.png" alt="Github Icon" className={styles.socialIcon}/></a></Button>
+          <Button className={styles.socialButton}><a href="https://devpost.com/Mmahan2" target="blank"><img src="http://simpleicon.com/wp-content/uploads/rocket.png" alt="Devpost Icon" className={styles.socialIcon}/></a></Button>
+        </div>
       </div>
     </div>
   )
